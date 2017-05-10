b43f6514599f56c1b20e1b1584598a829b4f3bef
Dabney <serenawindle@gmail.com>
2017-05-10 01:18:40 +0000

    Added simple menu display messages.

--- src/com/dabney/Menu.java
+++ src/com/dabney/Menu.java
@@ -1,7 +1,33 @@
 package com.dabney;
+import java.util.Scanner;
 
 /**
  * Created by soss on 5/8/17.
  */
 public class Menu {
+
+    Scanner input = new Scanner(System.in);
+
+    public void welcomeMessage() {
+        System.out.println("===================================");
+        System.out.println("===================================");
+        System.out.println("ROCK || PAPER || SCISSORS");
+        System.out.println("===================================");
+        System.out.println("===================================");
+    }
+
+    public void displayMainMenu() {
+        System.out.println("MAIN MENU");
+        System.out.println("----------------------------------");
+        System.out.println("1) play one-on-one fast match vs ai");
+        System.out.println("2) play one-on-one fast match vs player");
+        System.out.println("3) exit program");
+        System.out.println("----------------------------------");
+        System.out.println("WHAT WOULD YOU LIKE TO DO:");
+
+    }
+
+    private void displayError() {
+        System.out.println("You typed something you were not supposed to...");
+    }
 }
