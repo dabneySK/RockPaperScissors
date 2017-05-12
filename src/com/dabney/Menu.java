9a63f1b5c9780b399e7433da07352d0491adca5a
Dabney <serenawindle@gmail.com>
2017-05-12 16:35:55 +0000

    Added some base menu options.

--- src/com/dabney/Menu.java
+++ src/com/dabney/Menu.java
@@ -21,10 +21,32 @@ class Menu {
         System.out.println("3) exit program");
         System.out.println("----------------------------------");
         System.out.println("WHAT WOULD YOU LIKE TO DO:");
+        navigateMenu(getMenuChoice());
 
     }
 
     private void displayError() {
         System.out.println("You typed something you were not supposed to...");
+    }
+
+    public int getMenuChoice() {
+        int menuChoice = input.nextInt();
+        return menuChoice;
+    }
+
+    public void navigateMenu(int choice) {
+        switch(choice) {
+            case 0:
+                //one v one vs ai
+                break;
+            case 1:
+                // one v one vs player
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("Incorrect menu choice.");
+        }
+
     }
 }
