2f99e56d40cdc8ee9d07621491355f4652e8d60c
Dabney <serenawindle@gmail.com>
2017-07-12 16:48:05 +0000

    Began developing Player class

--- src/com/dabney/Player.java
+++ src/com/dabney/Player.java
@@ -1,4 +1,15 @@
 package com.dabney;
 
 class Player {
+    private String choices[] = {"Rock", "Paper", "Scissors"};
+    private String choice;
+    private int wins;
+
+    public String getChoice() {
+        return choice;
+    }
+
+    public int getWins() {
+        return wins;
+    }
 }
