d4c6be085950cdee1b33a30eefd87a77649b0508
Dabney <dabneysk@gmail.com>
2017-07-17 11:44:43 -0400

    Added getChoice for Computer class.

--- src/com/dabney/Computer.java
+++ src/com/dabney/Computer.java
@@ -2,10 +2,24 @@ package com.dabney;
 
 
 class Computer extends Player {
-
-
-    public int rollRandomNumber() {
+    
+    private int rollRandomNumber() {
         return (int) (Math.round( Math.random() * 2 ));
+    }
+
+    @Override
+    public String getChoice() {
+
+        switch(rollRandomNumber()) {
+            case 0:
+                return "Rock";
+            case 1:
+                return "Paper";
+            case 2:
+                return "Scissors";
+            default:
+                return null;
+        }
     }
 
 }
