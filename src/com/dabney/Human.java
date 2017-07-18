1fc61e52f5953784b79494e6c0787b4f6d375b2f
Dabney <serenawindle@gmail.com>
2017-07-18 12:08:59 -0400

    Added setChoice for Human class

--- src/com/dabney/Human.java
+++ src/com/dabney/Human.java
@@ -1,10 +1,14 @@
 package com.dabney;
+import java.util.Scanner;
 
 class Human extends Player {
 
+    private static Scanner input = new Scanner(System.in);
 
     @Override
     public void setChoice() {
-        // stub
+        System.out.println("What is your choice?");
+        System.out.println("Rock\nPaper\nScissors");
+        choice = input.nextLine();
     }
 }
