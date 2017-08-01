28ffe93d2bf44c8ba5cebb05e6818f93eaeeef5d
Dabney <serenawindle@gmail.com>
2017-08-01 18:31:17 -0400

    Removed testing code in main

--- src/com/dabney/RockPaperScissors.java
+++ src/com/dabney/RockPaperScissors.java
@@ -3,7 +3,6 @@ package com.dabney;
 public class RockPaperScissors {
 
     public static void main(String[] args) {
-        Menu menuxx = new Menu();
-        menuxx.setMenuChoice();
+        
     }
 }
